95ddef9db53c8b895f2c085ba6399a3144a4f8e6
Domino2357 <JustinBuerger@googlemail.com>
2021-09-27 12:27:41 +0200

    Solution to #22

--- #22.py
+++ #22.py
@@ -12,3 +12,32 @@ Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string
 return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 
 """
+# assuming no complexity bounds; assuming words can be part of the string multiple times
+# first thought: take all the words which are a prefix of the string; strip the prefix, do the same until the string is
+# empty
+
+
+def return_words(dictionary, word_bundle):
+    if word_bundle == '':
+        return []
+    for word in dictionary:
+        if starts_with(word_bundle, word):
+            next_word = return_words(dictionary, word_bundle[len(word):])
+            if next_word is not None:
+                return [word] + next_word
+            else:
+                return None
+
+
+# seems in python 3.7 there is no str.startswith(prefix)
+def starts_with(word, prefix):
+    if prefix == word[:len(prefix)]:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(return_words(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"))
+    print(return_words(['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"))
+    print(return_words(['hello', 'green', 'hopr'], "hellogreenhope"))
